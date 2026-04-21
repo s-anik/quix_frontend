a775a39465162c6680df7e11acc2ea103735f3e9
s-anik <anik15-4922@diu.edu.bd>
2026-04-21 13:38:18 +0600

    /api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "quixbackend-production.up.railway.app"
+    baseURL: "quixbackend-production.up.railway.app/api"
 });
 console.log(import.meta.env.VITE_API_URL);
 
